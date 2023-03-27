ada0d49b5a3388891ba5eab4a33379652aa94a59
hanny310 <hannyroh310@gmail.com>
2023-03-27 12:24:45 +0900

    #16-controller생성 및 mvc test추가

---
+++ project-board/src/test/java/com/fast/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,16 @@
+package com.fast.projectboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+}
